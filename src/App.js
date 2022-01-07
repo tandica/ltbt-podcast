b9e158845b47a8b945336b2281496dac8497b839
tandica <williams.tandica@gmail.com>
2022-01-06 23:26:06 -0500

    display text on page to make router work properly and install dependencies

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import "./styles/App.scss";
 
-export default function App() {
+function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <div>
+          <nav>
+            <ul>
+              {/* <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact Us</Link>
+              </li> */}
+            </ul>
+          </nav>
+        </div>
+      </Switch>
     </Router>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
+export default App;
