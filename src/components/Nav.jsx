27402b58a028f66b1cda462c2becf2e6768e6fc5
tandica <williams.tandica@gmail.com>
2022-01-07 23:03:42 -0500

    add simple styling to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,16 @@
+import "../styles/Nav.scss";
+
 export default function Nav() {
   return (
-    <div>
-      <a>EPISODES</a>
-      <a>EVENTS</a>
-      <a>ABOUT US</a>
-      <a>STORE</a>
-      <a>CONTACT US</a>
+    <div className="nav">
+      <div className="nav-logo">LTBT</div>
+      <div className="nav-items">
+        <a>EPISODES</a>
+        <a>EVENTS</a>
+        <a>ABOUT US</a>
+        <a>STORE</a>
+        <a>CONTACT US</a>
+      </div>
     </div>
   );
 }
