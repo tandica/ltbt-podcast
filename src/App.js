cc2a1d59bc99f67cd9e11390ad48b7a6dd7eb327
tandica <williams.tandica@gmail.com>
2022-01-08 22:38:11 -0500

    set up episodes page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Episode from "./components/Episodes";
 import "./styles/App.scss";
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
       <Switch>
         <Route path="/" exact>
           <Home />
+        </Route>
+        <Route path="/episodes" exact>
+          <Episode />
         </Route>
         <div>
           <nav>
---
+++ src/components/Episodes.jsx
@@ -0,0 +1,3 @@
+export default function Episodes() {
+  return <div>EPISODE PAGE</div>;
+}
