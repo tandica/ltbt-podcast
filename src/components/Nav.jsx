8a9eb4dbefdc7ac468f9ad72823a1a1c1c4ebf50
tandica <williams.tandica@gmail.com>
2022-01-11 18:41:56 -0500

    update nav links and styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,11 +5,11 @@ export default function Nav() {
     <div className="nav">
       <div className="nav-logo">LTBT</div>
       <div className="nav-items">
-        <a>EPISODES</a>
-        <a>EVENTS</a>
-        <a>ABOUT US</a>
-        <a>STORE</a>
-        <a>CONTACT US</a>
+        <a href="/episodes">EPISODES</a>
+        <a href="/episodes">EVENTS</a>
+        <a href="/episodes">ABOUT US</a>
+        <a href="/episodes">STORE</a>
+        <a href="/episodes">CONTACT US</a>
       </div>
     </div>
   );
