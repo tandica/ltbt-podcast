cddddcd9bdf1a72fb4f8b56ee7181d30186d7842
tandica <williams.tandica@gmail.com>
2022-01-11 22:09:46 -0500

    make data for shows appear on page

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -2,6 +2,7 @@ import Nav from "./Nav";
 import Footer from "./Footer";
 import "../styles/Events.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { upcomingEvents, upcomingLiveShows } from "../data";
 
 export default function Events() {
   return (
@@ -11,6 +12,13 @@ export default function Events() {
       </div>
       <div className="events-container">
         <h1 className="events-title">UPCOMING LIVE SHOWS</h1>
+        <div className="events-cards">
+          {upcomingLiveShows.map((show) => (
+            <h2>{show.title}</h2>
+
+            // <h4>{show.date}</h4>
+          ))}
+        </div>
         <h1 className="events-title2">UPCOMING EVENTS</h1>
       </div>
       <Footer />
