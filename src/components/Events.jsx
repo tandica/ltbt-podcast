947635f58971e34ccc5acbdb5d11a7f5e493b2fd
tandica <williams.tandica@gmail.com>
2022-01-11 22:53:14 -0500

    add preliminary styling for upcoming live shows

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -12,14 +12,25 @@ export default function Events() {
       </div>
       <div className="events-container">
         <h1 className="events-title">UPCOMING LIVE SHOWS</h1>
-        <div className="events-cards">
+        <div className="events-liveshow-container">
           {upcomingLiveShows.map((show) => (
-            <h2>{show.title}</h2>
-
-            // <h4>{show.date}</h4>
+            <div className="events-liveshow-card">
+              <h3 className="events-liveshow-title">{show.title}</h3>
+              <h4 className="events-liveshow-date">{show.date}</h4>
+              <h4 className="events-liveshow-location">{show.location}</h4>
+            </div>
           ))}
         </div>
         <h1 className="events-title2">UPCOMING EVENTS</h1>
+        <div className="upcoming-event-container">
+          {upcomingEvents.map((show) => (
+            <div className="upcoming-event-card">
+              <h3 className="upcoming-event-title">{show.title}</h3>
+              <h4 className="upcoming-event-date">{show.date}</h4>
+              <h4 className="upcoming-event-info">{show.info}</h4>
+            </div>
+          ))}
+        </div>
       </div>
       <Footer />
     </div>
