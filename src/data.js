23728c0d418d41fb03a34397f5cefd89c9a4460c
tandica <williams.tandica@gmail.com>
2022-01-11 21:34:44 -0500

    add data content to data file

--- src/data.js
+++ src/data.js
@@ -1,3 +1,45 @@
-export const upcomingLiveShows = [];
+export const upcomingLiveShows = [
+  {
+    date: "August 3, 2022",
+    title: "Title of Show",
+    location: "Toronto, Ontario",
+  },
+  {
+    date: "August 4, 2022",
+    title: "Title of Show",
+    location: "Toronto, Ontario",
+  },
+  {
+    date: "August 3, 2022",
+    title: "Title of Show",
+    location: "Toronto, Ontario",
+  },
+];
 
-export const upcomingEvents = [];
+export const upcomingEvents = [
+  {
+    title: "Black Girl Magic",
+    date: "September 3, 2022",
+    info: "Event Information",
+  },
+  {
+    title: "Herstory",
+    date: "September 7, 2022",
+    info: "Event Information",
+  },
+  {
+    title: "Blackity Black Black",
+    date: "September 3, 2022",
+    info: "Event Information",
+  },
+  {
+    title: "Women in Poetry",
+    date: "September 3, 2022",
+    info: "Event Information",
+  },
+  {
+    title: "For, To, and By the People",
+    date: "September 3, 2022",
+    info: "Event Information",
+  },
+];
