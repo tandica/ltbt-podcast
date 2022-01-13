49dfaa39e70ee56f9664e717139f122a62cae3db
tandica <williams.tandica@gmail.com>
2022-01-12 22:27:53 -0500

    add header image and fix nav logo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,13 @@
+import Nav from "./Nav";
+import "../styles/About.scss";
+
 export default function About() {
-  return <div>about page</div>;
+  return (
+    <div>
+      <div className="about-header-image">
+        <Nav />
+      </div>
+      about page
+    </div>
+  );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,9 @@ import "../styles/Nav.scss";
 export default function Nav() {
   return (
     <div className="nav">
-      <div className="nav-logo">LTBT</div>
+      <div className="nav-logo">
+        <a href="/">LTBT</a>
+      </div>
       <div className="nav-items">
         <a href="/episodes">EPISODES</a>
         <a href="/events">EVENTS</a>
