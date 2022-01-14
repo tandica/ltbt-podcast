a65cf96c514e40d0e0c12e1b09c3fb515a0043cf
tandica <williams.tandica@gmail.com>
2022-01-13 19:59:28 -0500

    add preliminary content to about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,26 @@ export default function About() {
       <div className="about-header-image">
         <Nav />
       </div>
-      about page
+      <div className="about-who-container">
+        <div className="about-who">
+          <h1 className="about-who-title">WHO ARE WE?</h1>
+          <p className="about-who-paragraph">
+            Let's Talk Big Tingz is an organization that was initiated to create
+            a safe space for young Black women to speak their truth, existence,
+            struggles, challenges, and successes. What began as a small idea,
+            LTBT has flourished into something bigger. Let's Talk Big Tingz
+            first debuted as a podcast where they talked about topics like
+            racism, Black women struggles, relationships, friendships,
+            discrimination, islamophobia... hence the title "Let's Talk Big
+            Tingz." These discussions turned into{" "}
+          </p>
+        </div>
+        <div className="about-who-img">
+          <img src="../assets/placeholder-image.png" alt="yhtyhyt" />
+          <img src="../assets/2.png" alt="yhtyhyt" />
+          <img src="assets/3.png" alt="yhtyhyt" />
+        </div>
+      </div>
     </div>
   );
 }
