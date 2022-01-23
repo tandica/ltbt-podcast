2040b8604d0749817f3424e82ef26a6092cdd99c
tandica <williams.tandica@gmail.com>
2022-01-23 18:17:17 -0500

    add preliminary styling to store paghe

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,11 +1,17 @@
 import "../styles/Store.scss";
+import Nav from "./Nav";
 import Footer from "./Footer";
 
 export default function Store() {
   return (
     <div>
+      <div className="store-header-image">
+        <Nav />
+      </div>
+      <div className="store-container">
+        <h1 className="store-title">SHOP OUR MERCH</h1>
+      </div>
       <Footer />
-      <h1>STOREEEE HIiii</h1>
     </div>
   );
 }
