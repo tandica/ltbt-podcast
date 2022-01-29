7fdd6e86bae2df579f4f7c7a98edf7f0cd18c85b
tandica <williams.tandica@gmail.com>
2022-01-29 00:09:09 -0500

    update folder structure

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import "../styles/Sidebar.scss";
+
+export default function Sidebar() {
+  // function openMenu() {
+  //   document.querySelector(".sidebar").classList.add("open");
+  // }
+  // return (
+  //   <div>
+  //     <aside className="sidebar">
+  //       <h3>EXPLORE OUR CATEGORIES</h3>
+  //       <ul>
+  //         <li>
+  //           <a href="/events">Sweaters</a>
+  //         </li>
+  //         <li>
+  //           <a href="/events">Stickers</a>
+  //         </li>
+  //       </ul>
+  //     </aside>
+  //     <button className="sidebar-button" onClick={openMenu}>
+  //       &#9776;
+  //     </button>
+  //   </div>
+  // );
+}
+
+//Add to public index file instead
---
+++ frontend/src/components/Store.jsx
@@ -0,0 +1,43 @@
+import "../styles/Store.scss";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import image from "../assets/1.png";
+import Sidebar from "./Sidebar";
+
+export default function Store() {
+  return (
+    <div>
+      <div className="store-header-image">
+        <Nav />
+      </div>
+      <div className="store-container">
+        <Sidebar />
+        {/* <aside className="sidebar">
+          <h3>EXPLORE OUR CATEGORIES</h3>
+          <ul>
+            <li>
+              <a href="/events">Sweaters</a>
+            </li>
+            <li>
+              <a href="/events">Stickers</a>
+            </li>
+          </ul>
+        </aside> */}
+        <h1 className="store-title">SHOP OUR MERCH</h1>
+        {/* <button className="sidebar-button" onClick="openMenu()">&#9776;</button> */}
+        <div className="products-container">
+          <ul className="products-list">
+            <li className="product">
+              {" "}
+              <img className="about-team-img" src={image} alt="person" />
+              <div className="product-name">Product Name</div>
+              <div className="product-price">$77</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+//47 mins at video
