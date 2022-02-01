061315f29768fa46cfbfb6c33c61c0f39826f7aa
tandica <williams.tandica@gmail.com>
2022-01-31 19:41:22 -0500

    make single product page dynamic

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,13 @@
 import "../styles/Product.scss";
+import { productData } from "../data";
+import { useParams } from "react-router-dom";
 
-export default function Product() {
-  return <div>Indiv product</div>;
+export default function Product(props) {
+  const { id } = useParams();
+  console.log(id);
+
+  const product = productData.products.find(
+    (productId) => productId.id === Number(id)
+  );
+  return <div>{product.name}</div>;
 }
