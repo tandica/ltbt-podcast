ea27a3ab6efc0f6a33a091ca6fef50a9ff630722
tandica <williams.tandica@gmail.com>
2022-02-27 23:03:59 -0500

    add redux to product details page

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,6 +5,7 @@ import {
 } from "../constants/productConstants";
 import axios from "axios";
 
+//view products on page
 const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
@@ -16,4 +17,17 @@ const listProducts = () => async (dispatch) => {
   }
 };
 
-export { listProducts };
+//view product details
+const detailsProduct = (productId) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+    const { data } = await axios.get(
+      "http://localhost:5000/api/store" + productId
+    );
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.message });
+  }
+};
+
+export { listProducts, detailsProduct };
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,15 +1,29 @@
 import "../styles/Product.scss";
 import { productData } from "../data";
 import { useParams, Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { detailsProduct } from "../actions/productActions";
 
 export default function Product(props) {
   //use this hook to get product id
-  const { id } = useParams();
+  // const { id } = useParams();
 
   //if product id matches id from useParams hook, return product info
-  const product = productData.products.find(
-    (productId) => productId.id === Number(id)
-  );
+  // const product = productData.products.find(
+  //   (productId) => productId.id === Number(id)
+  // );
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { product, loading, error } = productDetails;
+  const dispatch = useDispatch;
+
+  useEffect(() => {
+    dispatch(detailsProduct());
+
+    return () => {};
+  }, []);
+
   return (
     <div>
       <div className="store-backlink">
@@ -17,28 +31,34 @@ export default function Product(props) {
           Back to LTBT STORE
         </Link>
       </div>
-      <div className="product-details-container">
-        <div className="product-details-image">
-          <img src={product.image} alt="ltbt merch" />
-        </div>
-        <div className="product-details-info">
-          <h1 className="product-details-name">{product.name}</h1>
-          <h5 className="product-details-price">$ {product.price}</h5>
-          <p className="product-details-description">{product.description}</p>
-        </div>
-        <div className="product-action">
-          <div className="product-qty">
-            QTY:{" "}
-            <select>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-            </select>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <div className="product-details-container">
+          <div className="product-details-image">
+            <img src={product.image} alt="ltbt merch" />
           </div>
-          <button className="product-cart-button">ADD TO CART</button>
+          <div className="product-details-info">
+            <h1 className="product-details-name">{product.name}</h1>
+            <h5 className="product-details-price">$ {product.price}</h5>
+            <p className="product-details-description">{product.description}</p>
+          </div>
+          <div className="product-action">
+            <div className="product-qty">
+              QTY:{" "}
+              <select>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>
+            </div>
+            <button className="product-cart-button">ADD TO CART</button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
