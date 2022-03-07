b19583cc827542b253b14b025e514ea8f14911dd
tandica <williams.tandica@gmail.com>
2022-03-06 19:16:12 -0500

    clean up code on product page and store, delete unused files and folders

--- frontend/src/components/ProductPage.jsx
+++ frontend/src/components/ProductPage.jsx
@@ -4,7 +4,6 @@ import { useParams, Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useContext, useEffect, useReducer } from "react";
 import axios from "axios";
-import { detailsProduct } from "../actions/productActions";
 import LoadingBox from "./LoadingBox";
 import MessageBox from "./MessageBox";
 import { getError } from "../utils";
@@ -104,62 +103,3 @@ function ProductPage(props) {
 }
 
 export default ProductPage;
-
-//use this hook to get product id
-// const { id } = useParams();
-
-//if product id matches id from useParams hook, return product info
-// const product = productData.products.find(
-//   (productId) => productId.id === Number(id)
-// );
-
-//OLD CODE
-//   console.log("-----", props.match.params.id);
-//   const productDetails = useSelector((state) => state.productDetails);
-//   const { product, loading, error } = productDetails;
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(detailsProduct(props.match.params.id));
-
-//     return () => {};
-//   }, []);
-
-//   return (
-//     <div>
-//       <div className="store-backlink">
-//         <Link to="/store" className="store-backlink">
-//           Back to LTBT STORE
-//         </Link>
-//       </div>
-//       {loading ? (
-//         <div>Loading...</div>
-//       ) : error ? (
-//         <div>{error}</div>
-//       ) : (
-//         <div className="product-details-container">
-//           <div className="product-details-image">
-//             <img src={product.image} alt="ltbt merch" />
-//           </div>
-//           <div className="product-details-info">
-//             <h1 className="product-details-name">{product.name}</h1>
-//             <h5 className="product-details-price">$ {product.price}</h5>
-//             <p className="product-details-description">{product.description}</p>
-//           </div>
-//           <div className="product-action">
-//             <div className="product-qty">
-//               QTY:{" "}
-//               <select>
-//                 <option>1</option>
-//                 <option>2</option>
-//                 <option>3</option>
-//                 <option>4</option>
-//               </select>
-//             </div>
-//             <button className="product-cart-button">ADD TO CART</button>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
--- frontend/src/components/Store.jsx
+++ frontend/src/components/Store.jsx
@@ -6,14 +6,8 @@ import Footer from "./Footer";
 import Product from "./Product";
 import LoadingBox from "./LoadingBox";
 import MessageBox from "./MessageBox";
-// import image from "../assets/1.png";
-import Sidebar from "./Sidebar";
-// import { productData } from "../data";
 import { useState, useEffect, useReducer } from "react";
 import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
-import { listProducts } from "../actions/productActions";
-//import productData from "../data/data";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -76,87 +70,3 @@ function Store() {
 }
 
 export default Store;
-
-// const [products, setProduct] = useState([]);
-
-//OLD CODE
-// const productList = useSelector((state) => state.productList);
-// const { products, loading, error } = productList;
-
-// const dispatch = useDispatch();
-
-// useEffect(() => {
-//   dispatch(listProducts());
-
-//   return () => {};
-// }, []);
-
-//OLD OLD CODE
-// useEffect(() => {
-//   // axios.get("/store").then((data) => {
-//   //   setProduct(data);
-//   // });
-
-//   //the one that worked
-//   const fetchData = async () => {
-//     const { data } = await axios.get("http://localhost:5000/api/store");
-//     setProduct(data);
-//     // console.log("here is data", data);
-//   };
-//   fetchData();
-// }, []);
-
-//OLD CODE
-//   return loading ? (
-//     <div>Loading...</div>
-//   ) : error ? (
-//     <div>{error}</div>
-//   ) : (
-//     <div>
-//       <div className="store-header-image">
-//         <Nav />
-//       </div>
-//       <div className="store-container">
-//         {/* <Sidebar /> */}
-//         {/* <aside className="sidebar">
-//           <h3>EXPLORE OUR CATEGORIES</h3>
-//           <ul>
-//           <li>
-//           <a href="/events">Sweaters</a>
-//           </li>
-//           <li>
-//           <a href="/events">Stickers</a>
-//           </li>
-//           </ul>
-//         </aside> */}
-//         <h1 className="store-title">SHOP OUR MERCH</h1>
-//         {/* <button className="sidebar-button" onClick="openMenu()">&#9776;</button> */}
-//         <div className="products-container">
-//           <div className="products-list">
-//             {products.map((product) => (
-//               <div key={product.id} className="product">
-//                 {" "}
-//                 <Link to={"/store/" + product.id}>
-//                   {" "}
-//                   <img
-//                     className="about-team-img"
-//                     src={product.image}
-//                     alt="ltbt merch"
-//                   />
-//                 </Link>
-//                 <div className="product-name">
-//                   {" "}
-//                   <Link to={"/store/" + product.id} className="product-name">
-//                     {product.name}
-//                   </Link>
-//                 </div>
-//                 <div className="product-price">$ {product.price}</div>
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// }
